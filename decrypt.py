d5345ac9a2f9d0a58224e5b4e82781b3c3ce030a
Pi <knuekim98@naver.com>
2023-07-20 09:30:49 +0900

    feat:plot

--- decrypt.py
+++ decrypt.py
@@ -3,6 +3,7 @@ from const import data_folder, book_name, language
 from math import log, exp
 from jamo import h2j, j2hcj
 from hangul_utils import join_jamos
+import matplotlib.pyplot as plt
 
 
 with open(f'./{data_folder}/chain', 'r', encoding='UTF8') as f:
@@ -27,6 +28,57 @@ def get_possibility(dec):
         v += log(chain[dec[i]][dec[i+1]] + exp(-25))
     
     return v
+
+
+def mcmc(x, c1, verbose=True, write=True):
+    random.shuffle(x)
+
+    df = dict()
+    for i in range(len(c1)):
+        df[c1[i]] = x[i]
+    pos = get_possibility(get_dec(df))
+
+    if verbose:
+        print(df)
+        print(pos)
+
+    jump = 0
+    pos_plot = []
+    for k in range(10000):
+        r1 = random.randrange(0, len(c1))
+        r2 = r1
+        while r2 == r1: r2 = random.randrange(0, len(c1))
+
+        pos = get_possibility(get_dec(df))
+        new_df = df.copy()
+        new_df[c1[r2]] = df[c1[r1]]
+        new_df[c1[r1]] = df[c1[r2]]
+        new_pos = get_possibility(get_dec(new_df))
+
+        if new_pos > pos: 
+            pos = new_pos
+            df = new_df
+        elif new_pos - pos > log(random.random()): 
+            pos = new_pos
+            df = new_df
+            jump += 1
+
+        if k%200 == 0: pos_plot.append(pos)
+
+    if language == 'kor': result = join_jamos(get_dec(df))
+    else: result = get_dec(df)
+    print(result)
+
+    if verbose:
+        print(df)
+        print(pos)
+        print(jump)
+
+    if write:
+        with open(f'./{data_folder}/deciphertext', 'w', encoding='UTF8') as f:
+            f.write(result)
+
+    return pos_plot
 
 
 c1 = list(set(c))
@@ -59,46 +111,27 @@ x.reverse()
 x = x[:len(c1)]
 
 
-random.shuffle(x)
+loop = 50
 
-df = dict()
-for i in range(len(c1)):
-    df[c1[i]] = x[i]
+avg_plot = [0]*50
+result_plot = [0]*loop
+for _ in range(loop):
+    print(f'loop {_+1}/{loop}')
+    pos_plot = mcmc(x, c1, False, False)
+    result_plot[_] = pos_plot[-1]
+    for i in range(50):
+        avg_plot[i] += pos_plot[i]
+    
 
-dec = get_dec(df)
-pos = get_possibility(dec)
+plt.subplot(1, 2, 1)
+plt.plot([i*200 for i in range(50)], [p/loop for p in avg_plot], '.-')
+plt.xlabel('iterations')
+plt.ylabel('log possibility')
 
-print(df)
-print(pos)
+plt.subplot(1, 2, 2)
+plt.bar([i+1 for i in range(loop)], result_plot)
+plt.xlabel('loop')
+plt.ylabel('final log possibility')
 
-
-jump = 0
-for k in range(50000):
-    r1 = random.randrange(0, len(c1))
-    r2 = r1
-    while r2 == r1: r2 = random.randrange(0, len(c1))
-
-    pos = get_possibility(get_dec(df))
-    new_df = df.copy()
-    new_df[c1[r2]] = df[c1[r1]]
-    new_df[c1[r1]] = df[c1[r2]]
-    new_pos = get_possibility(get_dec(new_df))
-
-    if new_pos > pos: 
-        pos = new_pos
-        df = new_df
-    elif new_pos - pos > log(random.random()): 
-        pos = new_pos
-        df = new_df
-        jump += 1
-
-print(df)
-print(pos)
-print(jump)
-
-if language == 'kor': result = join_jamos(get_dec(df))
-else: result = get_dec(df)
-
-print(result)
-with open(f'./{data_folder}/deciphertext', 'w', encoding='UTF8') as f:
-    f.write(result)
+plt.tight_layout()
+plt.show()
